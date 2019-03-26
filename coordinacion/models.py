cd722a366535393fadf68ed39117d603631c44cf
Iván Moreno <if.moreno905@uniandes.edu.co>
2019-03-26 10:37:01 -0500

    prueba 2

--- coordinacion/models.py
+++ coordinacion/models.py
@@ -13,8 +13,10 @@ Juego de coordinacion del link más débil.
 
 class Constants(BaseConstants):
     name_in_url = 'coordinacion'
-    players_per_group = 4
-    num_rounds = 12
+#    players_per_group = 4
+    players_per_group = 2
+    #num_rounds = 12
+    num_rounds = 6
     endowment = 10
     multiplier = 0.5
     bono = 0.75
